a4781ffbbbd3672060214ae5796d37493230ac61
pingbowen <pingbowen23@163.com>
2024-02-27 11:34:52 +0800

    migarate

---
+++ run.sh
@@ -0,0 +1,17 @@
+MODEL_SAVE_DIR=save/
+
+mkdir -p $MODEL_SAVE_DIR
+
+
+CUDA_VISIBLE_DEVICES=0,1 python \
+    bitdelta/train.py \
+    --base_model /data/public/opensource_models/meta-llama/Llama-2-7b-hf/ \
+    --finetuned_model /data/public/opensource_models/meta-llama/Llama-2-7b-chat-hf/ \
+    --save_dir $MODEL_SAVE_DIR \
+    --batch_size 4 \
+    --num_steps 200 \
+    --save_num 0\
+    --save_full_model True
+
+    # --layers "layers.5."\
+    # /data/public/opensource_models/WizardLM/WizardMath-7B-V1.0/
